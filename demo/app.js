4ad58d1e83cb53e6eeb569047a89d35f6f48257a
Ashik Meerankutty <ashik9591@gmail.com>
2021-06-27 10:58:45 +0530

    Adding a basic UI for demo

--- demo/app.js
+++ demo/app.js
@@ -3,14 +3,62 @@ import { Popover } from '../lib';
 
 export default function App() {
   const [show, setShow] = useState(false);
+  const [position, setPosition] = useState('bottom');
+  const [align, setAlign] = useState();
 
   return (
     <div className="App">
-      <Popover show={show} content={<div>Hello</div>}>
-        <button type="button" onClick={() => setShow(!show)}>
-          Show popover
+      <div
+        style={{
+          display: 'grid',
+          height: '300px',
+          width: '100%',
+          placeItems: 'center',
+        }}
+      >
+        <Popover
+          show={show}
+          position={position}
+          align={align}
+          content={<div>Hello</div>}
+        >
+          <div>
+            <button type="button" onClick={() => setShow(!show)}>
+              Show popover
+            </button>
+          </div>
+        </Popover>
+      </div>
+      <div
+        style={{
+          position: 'fixed',
+          bottom: '10px',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <button type="button" onClick={() => setPosition('top')}>
+          Top
         </button>
-      </Popover>
+        <button type="button" onClick={() => setPosition('left')}>
+          Left
+        </button>
+        <button type="button" onClick={() => setPosition('bottom')}>
+          Bottom
+        </button>
+        <button type="button" onClick={() => setPosition('right')}>
+          Right
+        </button>
+        <button type="button" onClick={() => setAlign('start')}>
+          Start
+        </button>
+        <button type="button" onClick={() => setAlign('end')}>
+          End
+        </button>
+        <button type="button" onClick={() => setAlign()}>
+          Center
+        </button>
+      </div>
     </div>
   );
 }
